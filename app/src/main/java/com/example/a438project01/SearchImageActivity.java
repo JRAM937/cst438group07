9ec57f24057caa0c4c30d7de476b80e4a04fba12
ALiang <aliang@csumb.edu>
2021-02-04 12:49:27 -0800

    My branch for searching images.

---
+++ app/src/main/java/com/example/a438project01/SearchImageActivity.java
@@ -0,0 +1,13 @@
+package com.example.a438project01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchImageActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
