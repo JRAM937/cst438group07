34534b7049725adf9ff61a98be080171c3b9ee00
Arthur Timpe <arthurtimpe@Arthurs-MacBook-Air.local>
2021-02-11 13:28:03 -0800

    added create account activity

--- app/src/main/java/com/example/a438project01/MainActivity.java
+++ app/src/main/java/com/example/a438project01/MainActivity.java
@@ -10,5 +10,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
+=======
+
+        //Set title
+        setTitle("PicPanda - Welcome!");
+
+        wireupButtons();
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = LoginActivity.intentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+
+        createAccButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = CreateAccountActivity.intentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void wireupButtons() {
+        searchButton = findViewById(R.id.search_button);
+        loginButton = findViewById(R.id.login_button);
+        createAccButton = findViewById(R.id.create_account_button);
+    }
+
+    public static Intent intentFactory(Context context) {
+        return new Intent(context, MainActivity.class);
+>>>>>>> Stashed changes
     }
 }
