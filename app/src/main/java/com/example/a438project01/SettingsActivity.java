b6a115000abaa535e391eb85c1fd3a03e32cce8c
Juan Ramirez JR <43487925+JRAM937@users.noreply.github.com>
2021-02-11 17:12:08 -0800

    -Delete Account button now takes you to the main Activity

--- app/src/main/java/com/example/a438project01/SettingsActivity.java
+++ app/src/main/java/com/example/a438project01/SettingsActivity.java
@@ -1,5 +1,29 @@
 package com.example.a438project01;
 
-public class SettingsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_login);
+
+        setTitle("PicPanda - App Settings");
+
+        Button logoutButton = findViewById(R.id.delete_acc_button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainActivity.intentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+    }
 }
