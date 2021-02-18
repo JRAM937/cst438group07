cf4d53e382d6869f8615ada92e62426ce096bd28
Juan Ramirez JR <43487925+JRAM937@users.noreply.github.com>
2021-02-18 13:24:52 -0800

    -Remove the notis boolean from Main activity -noti is now in AccountDatabase

--- app/src/main/java/com/example/a438project01/SettingsActivity.java
+++ app/src/main/java/com/example/a438project01/SettingsActivity.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -88,12 +89,16 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        @SuppressLint("UseSwitchCompatOrMaterialCode") Switch notiSwitch = findViewById(R.id.noti_switch);
-
-        notiSwitch.setOnClickListener(new View.OnClickListener() {
+        //Logic for Notification On/Off switch
+       @SuppressLint("UseSwitchCompatOrMaterialCode") Switch notiSwitch = findViewById(R.id.noti_switch);
+        notiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                //Does Something
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //Set noti to true
+                } else{
+                    //set noti to false
+                }
             }
         });
 
